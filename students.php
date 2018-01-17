9fbc5269c0a928903036aba6e88dfef41dcc7b2c
Neelam JITENDRA <neelam_jitendra@yahoo.com>
2018-01-17 22:58:43 +0100

    Update students.php

--- students.php
+++ students.php
@@ -2,5 +2,14 @@
 	$jsondata = file_get_contents("students.json");
 	$json = json_decode($jsondata,true);
 	$output = "<ul>";
- 
+ 	foreach($json['students'] as $student)
+	{
+		$output .= "<li>".$student['name']."</li>";
+		$output .= "<li>age: ".$student['age']."</li>";
+		$output .= "<li>sex: ".$student['sex']."</li>";
+		$output .= "<li>nationality: ".$student['nationality']."</li>";
+		$output .= "<li>course: ".$student['course']."</li>";
+	}
+	$output .= "</ul>";
+	echo $output;
  ?>
